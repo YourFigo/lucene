d55d8fafb7d14ed0f82a013f7203608e6e45d21b
YourFigo <501385003@qq.com>
2019-12-19 22:18:39 +0800

    lucene 标准分析器 和 IKAnalyzer 分析器

--- lucene-demo/src/cn/figo/lucene/LuceneFirst.java
+++ lucene-demo/src/cn/figo/lucene/LuceneFirst.java
@@ -1,12 +1,17 @@
 package cn.figo.lucene;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.File;
 
@@ -24,8 +29,8 @@ public class LuceneFirst {
         //把索引库保存在磁盘
         Directory directory = FSDirectory.open(new File("D:\\1_Code\\java\\lucene\\temp\\index").toPath());
         //2、基于Directory对象创建一个IndexWriter对象
-//        IndexWriterConfig config = new IndexWriterConfig(new IKAnalyzer());
-        IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig());
+        IndexWriterConfig config = new IndexWriterConfig(new IKAnalyzer());
+        IndexWriter indexWriter = new IndexWriter(directory, config);
         //3、读取磁盘上的文件，对应每个文件创建一个文档对象。
         File dir = new File("D:\\1_Code\\java\\lucene\\searchsource");
         File[] files = dir.listFiles();
@@ -94,5 +99,25 @@ public class LuceneFirst {
         }
         //9、关闭IndexReader对象
         indexReader.close();
+    }
+
+    @Test
+    public void testTokenStream() throws Exception {
+        //1）创建一个Analyzer对象，StandardAnalyzer对象
+//        Analyzer analyzer = new StandardAnalyzer();
+        Analyzer analyzer = new IKAnalyzer();
+        //2）使用分析器对象的tokenStream方法获得一个TokenStream对象
+        TokenStream tokenStream = analyzer.tokenStream("", "2017年12月14日 - 传智播客Lucene概述公安局Lucene是一款高性能的、可扩展的信息检索(IR)工具库。信息检索是指文档搜索、文档内信息搜索或者文档相关的元数据搜索等操作。");
+//        TokenStream tokenStream = analyzer.tokenStream("", "The Spring Framework provides a comprehensive programming and configuration model.");
+        //3）向TokenStream对象中设置一个引用，相当于数一个指针
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        //4）调用TokenStream对象的rest方法，把指针调到最前。如果不调用抛异常
+        tokenStream.reset();
+        //5）使用while循环遍历TokenStream对象
+        while(tokenStream.incrementToken()) {
+            System.out.println(charTermAttribute.toString());
+        }
+        //6）关闭TokenStream对象
+        tokenStream.close();
     }
 }
